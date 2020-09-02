dd3d1124278d894babc6e65bf138e30ba8d17447
IrinaLukyanova <forks1998@mail.ru>
2020-09-02 15:38:07 +0300

    Day section realization

--- src/MainScreen/CalendarButton.js
+++ src/MainScreen/CalendarButton.js
@@ -4,8 +4,8 @@ import Calendar from "./Calendar";
 
 function CalendarButton() {
   return (
-    <div class="day-list__calendar-wrapper">
-      <button class="heading-btn days-list__calendar">10 JUNE 2020</button>
+    <div className="day-list__calendar-wrapper">
+      <button className="heading-btn days-list__calendar">10 JUNE 2020</button>
       <Calendar />
     </div>
   );
