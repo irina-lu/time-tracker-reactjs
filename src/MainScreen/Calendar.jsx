10b98e30b9df00345ccd0c9832e4c3e737d744b1
IrinaLukyanova <forks1998@mail.ru>
2020-09-27 17:14:05 +0300

    Create calendar

--- src/MainScreen/Calendar.jsx
+++ src/MainScreen/Calendar.jsx
@@ -1,67 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 import "./Calendar.scss";
 
-function Calendar() {
+function ReactCalendar() {
+  const [date, setDate] = useState(new Date());
+
+  function onChange(date) {
+    setDate(date);
+    console.log(date);
+  }
+
   return (
-    <section className="calendar">
-      <h2 className="calendar__month">June</h2>
-      <ul className="calendar__weekdays-list">
-        <li className="calendar__weekday">S</li>
-        <li className="calendar__weekday">M</li>
-        <li className="calendar__weekday">T</li>
-        <li className="calendar__weekday">W</li>
-        <li className="calendar__weekday">T</li>
-        <li className="calendar__weekday">F</li>
-        <li className="calendar__weekday">S</li>
-      </ul>
-      <ul className="calendar__days-list">
-        <li className="calendar__day calendar__day_another-month">29</li>
-        <li className="calendar__day calendar__day_another-month">30</li>
-        <li className="calendar__day calendar__day_another-month">31</li>
-        <li className="calendar__day">1</li>
-        <li className="calendar__day">2</li>
-        <li className="calendar__day">3</li>
-        <li className="calendar__day calendar__day_in-progress">4</li>
-        <li className="calendar__day calendar__day_missed">5</li>
-        <li className="calendar__day calendar__day_in-progress">6</li>
-        <li className="calendar__day">7</li>
-        <li className="calendar__day">8</li>
-        <li className="calendar__day">9</li>
-        <li className="calendar__day calendar__day_in-progress">10</li>
-        <li className="calendar__day calendar__day_missed">11</li>
-        <li className="calendar__day calendar__day_missed">12</li>
-        <li className="calendar__day calendar__day_missed">13</li>
-        <li className="calendar__day calendar__day_missed">14</li>
-        <li className="calendar__day calendar__day_missed">15</li>
-        <li className="calendar__day calendar__day_missed">16</li>
-        <li className="calendar__day">17</li>
-        <li className="calendar__day calendar__day_missed">18</li>
-        <li className="calendar__day calendar__day_in-progress">19</li>
-        <li className="calendar__day">20</li>
-        <li className="calendar__day">21</li>
-        <li className="calendar__day calendar__day_in-progress calendar__day_chosen">
-          22
-        </li>
-        <li className="calendar__day calendar__day_in-progress">23</li>
-        <li className="calendar__day">24</li>
-        <li className="calendar__day calendar__day_in-progress">25</li>
-        <li className="calendar__day">26</li>
-        <li className="calendar__day">27</li>
-        <li className="calendar__day">28</li>
-        <li className="calendar__day">29</li>
-        <li className="calendar__day">30</li>
-        <li className="calendar__day calendar__day_another-month">1</li>
-        <li className="calendar__day calendar__day_another-month">2</li>
-      </ul>
-      <div className="calendar__statuses-wrapper">
-        <p className="calendar__status">Done</p>
-        <p className="calendar__status calendar__status_in-progress">
-          In progress
-        </p>
-        <p className="calendar__status calendar__status_missed">Missed</p>
-      </div>
-    </section>
+    <Calendar
+      value={date}
+      onChange={onChange}
+      locale="EN"
+      calendarType="US"
+      minDetail={"month"}
+      formatShortWeekday={(locale, date) =>
+        ["S", "M", "T", "W", "T", "F", "S"][date.getDay()]
+      }
+      formatMonthYear={(locale, date) =>
+        [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ][date.getMonth()]
+      }
+    />
+
+    // <div className="calendar__statuses-wrapper">
+    //   <p className="calendar__status">Done</p>
+    //   <p className="calendar__status calendar__status_in-progress">
+    //     In progress
+    //   </p>
+    //   <p className="calendar__status calendar__status_missed">Missed</p>
+    // </div>
   );
 }
 
-export default Calendar;
+export default ReactCalendar;
--- src/MainScreen/CalendarButton.jsx
+++ src/MainScreen/CalendarButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import ReactCalendar from "./Calendar";
 import "./CalendarButton.scss";
-import Calendar from "./Calendar";
 
 function CalendarButton() {
   return (
     <div className="day-list__calendar-wrapper">
       <button className="heading-btn days-list__calendar">10 JUNE 2020</button>
-      <Calendar />
+      <ReactCalendar />
     </div>
   );
 }
