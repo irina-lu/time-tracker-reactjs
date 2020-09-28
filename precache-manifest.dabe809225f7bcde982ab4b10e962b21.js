a01bf972fe1464f0b8597de6155181c3824ec295
IrinaLukyanova <forks1998@mail.ru>
2020-09-28 22:25:54 +0300

    Updates

---
+++ precache-manifest.dabe809225f7bcde982ab4b10e962b21.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2eea0700b05da4dd7455426be5e490",
+    "url": "/time-tracker-reactjs/index.html"
+  },
+  {
+    "revision": "fe907c1aab3f6c527e8c",
+    "url": "/time-tracker-reactjs/static/css/main.08d61229.chunk.css"
+  },
+  {
+    "revision": "17a61df687d189712e65",
+    "url": "/time-tracker-reactjs/static/js/2.e9688e71.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/time-tracker-reactjs/static/js/2.e9688e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe907c1aab3f6c527e8c",
+    "url": "/time-tracker-reactjs/static/js/main.58317119.chunk.js"
+  },
+  {
+    "revision": "43233fe235ca204534e6",
+    "url": "/time-tracker-reactjs/static/js/runtime-main.6d70756f.js"
+  },
+  {
+    "revision": "f18a9e37f3aa9f52dd6e84a217f25af8",
+    "url": "/time-tracker-reactjs/static/media/btn-download.f18a9e37.svg"
+  },
+  {
+    "revision": "a533856087b1acd313feb3b1fb352c46",
+    "url": "/time-tracker-reactjs/static/media/calendar-gray.a5338560.svg"
+  },
+  {
+    "revision": "874355570a52a7610234c1d26bb505f1",
+    "url": "/time-tracker-reactjs/static/media/calendar.87435557.svg"
+  },
+  {
+    "revision": "e4ae8aa2eaeb3663bbe0ade9b6f4b7b9",
+    "url": "/time-tracker-reactjs/static/media/exit.e4ae8aa2.svg"
+  },
+  {
+    "revision": "084d740336fe0559f9787229e1783053",
+    "url": "/time-tracker-reactjs/static/media/info.084d7403.svg"
+  },
+  {
+    "revision": "0502902293d22181260e244a2005d9b0",
+    "url": "/time-tracker-reactjs/static/media/play-white.05029022.svg"
+  },
+  {
+    "revision": "58217e30a5644be68663c69adad84525",
+    "url": "/time-tracker-reactjs/static/media/play.58217e30.svg"
+  },
+  {
+    "revision": "bb64c5860047192ae03b8d1236ddf3ed",
+    "url": "/time-tracker-reactjs/static/media/settings.bb64c586.svg"
+  }
+]);
